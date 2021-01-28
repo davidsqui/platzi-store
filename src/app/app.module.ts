2ef955ad3c5fa04cf17e8cebfe0fc814e838f2d9
davidsqui <davidsqr6@gmail.com>
2021-01-27 22:54:19 -0500

    Initial project

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductComponent } from './components/product/product.component';
+import { CartComponent } from './components/cart/cart.component';
+import { ExponentialPipe } from './pipes/exponential.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductComponent,
+    CartComponent,
+    ExponentialPipe
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
